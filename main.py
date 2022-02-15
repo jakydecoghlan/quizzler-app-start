682b7c33fd1c21d9fbff5ce962058514dcf969d4
jakydecoghlan <jakymovil@gmail.com>
2022-02-15 19:32:42 -0300

    estudio

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ from ui import QuizInterface
 # print(question_data)
 
 
-
 question_bank = []
 for question in question_data:
     question_text = question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
-
 
 
 quiz = QuizBrain(question_bank)
--- ui.py
+++ ui.py
@@ -2,6 +2,8 @@ from tkinter import *
 from quiz_brain import QuizBrain
 THEME_COLOR = "#375362"
 
+import time
+
 
 class QuizInterface:
 
@@ -12,7 +14,7 @@ class QuizInterface:
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        self.canvas = Canvas(width=300, height=250)
+        self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(150, 125, width=280,
                                                      text="{QuizBrain.q_text}",
                                                      fill=THEME_COLOR,
@@ -30,11 +32,15 @@ class QuizInterface:
         self.score.config(padx=20, pady=20)
 
         true_image = PhotoImage(file="./images/true.png")
-        self.true_button = Button(text="Button", image=true_image, bg=THEME_COLOR, highlightthickness=0)
+        self.true_button = Button(text="Button", image=true_image, bg=THEME_COLOR, highlightthickness=0,
+                                  command=self.true_answer
+                                  )
         self.true_button.grid(column=0, row=2, padx=20, pady=20)
 
         false_image = PhotoImage(file="./images/false.png")
-        self.false_button = Button(text="Button", image=false_image, bg=THEME_COLOR, highlightthickness=0)
+        self.false_button = Button(text="Button", image=false_image, bg=THEME_COLOR, highlightthickness=0,
+                                   command=self.false_answer
+                                   )
         self.false_button.grid(column=1, row=2, padx=20, pady=20)
 
         self.get_next_question()
@@ -42,6 +48,27 @@ class QuizInterface:
         self.window.mainloop()
 
 
-        def get_next_question():
-            q_text = self.quiz.next_question()
-            self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+
+    def true_answer(self):
+        self.give_feedback(self.quiz.check_answer("true"))
+
+    def false_answer(self):
+        is_right = (self.quiz.check_answer("false"))
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+            self.window.after(1000, self.get_next_question)
+            # self.canvas.config(bg="white")
+        else:
+            self.canvas.config(bg="red")
+            self.window.after(1000, self.get_next_question)
+            #
+
+
